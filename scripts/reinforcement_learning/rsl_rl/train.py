23355a74ea1eae6233b9a25b14fbb0787990daf0
yelong shen <yeshe@microsoft.com>
2025-10-29 15:12:58 -0700

    change log

--- scripts/reinforcement_learning/rsl_rl/train.py
+++ scripts/reinforcement_learning/rsl_rl/train.py
@@ -115,6 +115,10 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     """Train with RSL-RL agent."""
     # override configurations with non-hydra CLI arguments
     agent_cfg = cli_args.update_rsl_rl_cfg(agent_cfg, args_cli)
+    print("\n===== Final Environment Configuration =====")
+    print_dict(env_cfg.to_dict(), nesting=4)
+    print("\n===== Final Agent Configuration =====")
+    
     env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
     agent_cfg.max_iterations = (
         args_cli.max_iterations if args_cli.max_iterations is not None else agent_cfg.max_iterations
